51a2e35a5829e3da5b66ff1e649c5664e92daa41
Gerardo <ant0ni0lnx31@gmail.com>
2025-11-18 17:16:23 -0600

    adding features of streamlit

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ import os
 from dotenv import load_dotenv
 from client_llm_logic import get_llm_client
 
+import streamlit as st
+
 load_dotenv()
+
+st.title("RAG")
+st.subheader("chat with your own documents")
 
 API_HOST = os.getenv("API_HOST", "groq").lower()
 
@@ -11,18 +16,49 @@ if API_HOST == "groq":
 elif API_HOST == "gemini":
     MODEL_TO_USE = "gemini-2.5-flash"
 
-PROMPT = "Escribe un historia de amor entre una pareja de distintas clases sociales en la epoca de los años 2000 en mexico"
 
-if __name__ == "__main__":
-    try:
-        client = get_llm_client()
-        print(f"Using {API_HOST} client with model: {MODEL_TO_USE}")
-        response = client.generate_content(PROMPT, MODEL_TO_USE)
+try:
+    client = get_llm_client()
+    print(f"Using {API_HOST} client with model: {MODEL_TO_USE}")
+    #full_story = ""
+    #for token in response:
+    #    full_story += token
+    #    print(token, end="", flush=True)
+except Exception as e:
+    print(f"\n[ERROR] an error occured: {e}")
 
-        full_story = ""
-        for token in response:
-            full_story += token
-            print(token, end="", flush=True)
-        print("\n\n--- End of story ---")
-    except Exception as e:
-        print(f"\n[ERROR] an error occured: {e}")
+
+# inizializate chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# display chat meesages from history and on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# accept user input
+if prompt := st.chat_input("What is up?"):
+    # add use message to chat history
+    st.session_state.messages.append({
+        "role": "user",
+        "content": prompt,
+    })
+    # display user message in chat message container
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    # display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        assistant_response = client.generate_content(prompt, MODEL_TO_USE)
+        for token in assistant_response:
+            full_response += token
+            message_placeholder.markdown(full_response)
+        message_placeholder.markdown(full_response)
+    # add assistant response to chat history
+    st.session_state.messages.append({
+        "role":"assistant",
+        "content":full_response,
+    })
