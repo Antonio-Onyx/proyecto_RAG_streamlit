09b0e6c4aca0a0ef2e6b5dc7c81547ada4406f0a
Gerardo <ant0ni0lnx31@gmail.com>
2025-11-18 19:45:51 -0600

    refactor: extract text splitting, embedding generation, and vector store creation into dedicated functions.

--- RAG_modules_logic.py
+++ RAG_modules_logic.py
@@ -5,37 +5,16 @@ import faiss
 from langchain_community.vectorstores import FAISS
 from langchain_community.docstore.in_memory import InMemoryDocstore
 
-def process_document(document):
+def process_document(document: str) -> FAISS:
     """
     RAG chain logic to process document
     """
-    text_splitter = CharacterTextSplitter(
-        chunk_size=5000,
-        chunk_overlap=500,
-        add_start_index=True,
-    )
-    loader = PyPDFLoader(document)
-    documents = loader.load()
-    print(f"Document laoded, number of pages: {len(documents)}")
+    chunks = text_splitter(document) #chunks -> embeddings
+    embeddings = generate_embeddings() # embeddings -> vector store
+    db = vector_store(embeddings=embeddings, documents=chunks) #vector store -> ids
+    return db
 
-    # generate embeddings
-    chunks = text_splitter.split_documents(documents)
-    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-
-    # vector store
-    embedding_dim = len(embeddings.embed_query(chunks[0].page_content))
-    index = faiss.IndexFlatL2(embedding_dim)
-
-    vector_store = FAISS(
-        embedding_function=embeddings,
-        index=index,
-        docstore=InMemoryDocstore(), # al docs save in memory ram
-        index_to_docstore_id={}
-    )
-    ids = vector_store.add_documents(documents=documents)
-
-def text_splitter(document):
+def text_splitter(document: str) -> list:
     text_splitter = CharacterTextSplitter(
         chunk_size=5000,
         chunk_overlap=500,
@@ -46,22 +25,25 @@ def text_splitter(document):
     chunks = text_splitter.split_documents(documents)
     return chunks
 
-def generate_embeddings(documents):
+def generate_embeddings() -> HuggingFaceEmbeddings:
     embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
     return embeddings
 
-def vector_store(embeddings, documents):
-    embedding_dim = len(embeddings.embed_query(chunks[0].page_content))
+def vector_store(embeddings: HuggingFaceEmbeddings, documents: list) -> FAISS:
+    dim_sample = embeddings.embed_query("Hello world")
+    embedding_dim = len(dim_sample)
     index = faiss.IndexFlatL2(embedding_dim)
 
-    vector_store = FAISS(
+    vector_store_obj = FAISS(
         embedding_function=embeddings,
         index=index,
         docstore=InMemoryDocstore(), # al docs save in memory ram
         index_to_docstore_id={}
     )
-    ids = vector_store.add_documents(documents=documents)
-    return ids
+    ids = vector_store_obj.add_documents(documents=documents)
+
+    return vector_store_obj
 
 if __name__ == "__main__":
-    process_document("./data/en_las_montañas_de_la_locura.pdf")
+    result = process_document("./data/en_las_montañas_de_la_locura.pdf")
+    print(f"Type: {type(result)}")
